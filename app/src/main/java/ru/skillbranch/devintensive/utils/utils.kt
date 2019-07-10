d05131d036641bd85423bb265d055f5b0b67be1d
Nerz <Nerz@users.noreply.github.com>
2019-07-10 14:00:30 +1000

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/utils.kt
@@ -0,0 +1,35 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName:String?):Pair<String?, String?> {
+
+        val parts: List<String>
+        var firstName: String? = ""
+        var lastName: String? = ""
+
+        if (fullName != null) {
+                if (fullName.contains(" ")) {
+                    parts = fullName.split(" ")
+                    if ((parts[0]!="")&&(parts[0]!=" ")) firstName = parts[0] else firstName = null
+                    if ((parts[1]!="")&&(parts[1]!=" ")) lastName = parts[1] else lastName = null
+                } else {
+                    if (fullName!="") firstName = fullName else firstName = null
+                    lastName = null
+                }
+        } else {
+            val firstName = null
+            val lastName = null
+        }
+        return Pair(firstName, lastName)
+    }
+
+
+
+    fun transliteration(payload: String, divider:String=" "): String {
+        TODO()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        TODO()
+    }
+}
